4aa1ce092e9244dcacd2d441fedd08cf4fd57ac7
personnameds <personnameds@gmail.com>
2018-01-14 09:47:57 -0500

    Google Authentication
    https://github.com/python-social-auth/social-app-django

--- confschedule/settings.py
+++ confschedule/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'schedule.apps.ScheduleConfig',
     'classlists.apps.ClasslistsConfig',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -88,6 +91,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+ 
+ 'django.contrib.auth.backends.ModelBackend',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
@@ -109,3 +119,6 @@ except ImportError:
 
 LOGIN_REDIRECT_URL='login'
 LOGIN_URL='login'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='815731549404-k3tg0i4ruhjtrhkm4j4rrven5co2odlv.apps.googleusercontent.com'  #Paste CLient Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'KaDhPeVZvl-XhhoAvMWeiJCg' #Paste Secret Key
--- confschedule/urls.py
+++ confschedule/urls.py
@@ -1,29 +1,43 @@
 """confschedule URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+""" Version 2.0 will need to check settings
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+"""
+
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
-from django.contrib.auth.views import logout
+#from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('homepage.urls')),
     url(r'^add/', include('classlists.urls')),
     url(r'^(?P<klass>\d+)/', include('schedule.urls')),
-    url(r'^logout/', logout,{'next_page':'/'}),
-    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, {'next_page':'/'}, name='logout'),
+    url(r'^auth/', include('social_django.urls', namespace='social')),
+#    url(r'^$', home, name='home'),
     url(r'^admin/', admin.site.urls),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
